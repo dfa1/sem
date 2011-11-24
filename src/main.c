aa67ab160f4016e88bc0909a1850b9f579695166
Davide Angelocola <davide.angelocola@gmail.com>
2011-11-24 22:08:32 +0100

    ref: main simplified

--- src/main.c
+++ src/main.c
@@ -24,39 +24,21 @@
 
 #include <getopt.h>
 
-/* Command line option flags. */
-#define VRS     	1 << 0	/* version      -v      */
-#define MMR     	1 << 1	/* memory size  -m      */
-#define STC     	1 << 2	/* stack size   -s      */
-#define DBG 		1 << 3	/* debug        -d      */
-
-/* Flags. */
-int flags = 0;
-
-/* Program name. */
-char *program;
-
-/* Print the usage message and exit. */
 static void usage(int sts) {
-    if (sts == EXIT_SUCCESS)
-	fprintf(sts ? stderr : stdout, "\r\
-Usage: %s [options] file\n\
+  FILE *target = (sts == EXIT_SUCCESS)? stdout : stderr;
+  
+  fprintf(target, "\r\
+Usage: sem [options] file\n			\
 \n\
 Options:\n\
-  -d      : debug mode\n\
   -h      : print this help message and exit\n\
   -m      : set the data memory size (the default is 64)\n\
   -s      : set the evaluation stack size (the default is 32)\n\
   -v      : print the version and exit\n\
 \n\
-Report bugs to <davide.angelocola@gmail.com>\n", program);
-    else
-	fprintf(sts ? stderr : stdout,
-		"Try `%s -h' for more information.\n", program);
-
-    exit(sts);
+Report bugs to <davide.angelocola@gmail.com>\n");
+  exit(sts);
 }
-
 
 int main(int argc, char **argv) {
     const char licenseMsg[] = "\r\
@@ -74,7 +56,6 @@ See the LICENSE for more details.\n";
     struct option long_options[] = {
 	{ "version", 0, 0, 'v' },
 	{ "help",    0, 0, 'h' },
-	{ NULL,      1, 0, 'd' },
 	{ NULL,      0, 0, 'm' },
 	{ NULL,      0, 0, 's' },	    
 
@@ -82,75 +63,51 @@ See the LICENSE for more details.\n";
 	{ 0,         0, 0, 0   }
     };
 
-    program = argv[0];
-
-    while ((opt = getopt_long(argc, argv, "dhm:s:v", long_options, NULL)) 
+    while ((opt = getopt_long(argc, argv, "hm:s:v", long_options, NULL)) 
 	   != EOF) {
 	switch (opt) {
-	case 'd':
-	    set(flags, DBG);
-	    break;
 
 	case 'h':
 	    usage(EXIT_SUCCESS);
 	    break;
 
 	case 'm':
-	    set(flags, MMR);
-	    break;
+	  if (sscanf(optarg, "%d", &memSize) != 1 || memSize < 1) {
+	    fprintf(stderr, "sem: invalid memory size (%s)\n", optarg);
+	    return EXIT_FAILURE;
+	  }
+	  break;
 
 	case 's':
-	    set(flags, STC);
-	    break;
+	  if (sscanf(optarg, "%d", &stackSize) != 1 || stackSize < 1) {
+	    fprintf(stderr, "sem: invalid stack size (%s)\n", optarg);
+	    return EXIT_FAILURE;
+	  }
+	  break;
 
 	case 'v':
-	    set(flags, VRS);
-	    break;
+	  fprintf(stdout, licenseMsg, VERSION);
+	  return EXIT_SUCCESS;
 
 	default:
-	    usage(EXIT_FAILURE);
-	}
-    }
-
-    if (with(flags, VRS)) {
-	fprintf(stdout, licenseMsg, VERSION);
-	return 0;
-    }
-
-    if (with(flags, MMR)) {
-	if (sscanf(optarg, "%d", &memSize) != 1 || memSize < 1) {
-	    fprintf(stderr, "sem: invalid memory size (%s)\n",
-		    optarg);
-	    return 1;
-	}
-    }
-
-    if (with(flags, STC)) {
-	if (sscanf(optarg, "%d", &stackSize) != 1 || stackSize < 1) {
-	    fprintf(stderr, "sem: invalid stack size (%s)\n", optarg);
-	    return 1;
+	  fprintf(stderr, "Try `sem -h' for more information.\n");
+	  return EXIT_FAILURE;
 	}
     }
 
     if (optind < argc && strcmp(argv[optind], "-") == 0) {
       fprintf(stderr, "sem: no input\n");
-      return 1;
+      return EXIT_FAILURE;
     }
     
     struct Code *code = compileSource(argv[optind]);
     
     if (code == NULL) {
-      fprintf(stderr, "sem: error during compile\n");
-      return 1;
+      // error message should be already displayed
+      return EXIT_FAILURE;
     }
 
     struct VM *vm= initVM(code, memSize, stackSize);
-	
-    if (with(flags, DBG)) {
-      fprintf(stdout, "TODO: unsupported\n");
-      return 1;
-    }
-
     int status = evalCode(vm);
     finiCompiler(code);
     finiVM(vm);
