3bb6c877eb3c567c2ce257e51226e651a2ade04c
Davide Angelocola <davide.angelocola@gmail.com>
2013-01-13 10:52:15 +0100

    ref: dropping autogenerated scanner.h file

--- src/tokens.h
+++ src/tokens.h
@@ -1,22 +1,21 @@
-   enum yytokentype {
-     kSET = 258,
-     kHALT = 259,
-     kJUMP = 260,
-     kJUMPT = 261,
-     rD = 262,
-     rIP = 263,
-     rREAD = 264,
-     rWRITE = 265,
-     rWRITELN = 266,
-     tINT = 267,
-     tSTRING = 268,
-     tNAME = 269,
-     tEQ = 270,
-     tNE = 271,
-     tGT = 272,
-     tLT = 273,
-     tGE = 274,
-     tLE = 275,
-     tNEWLINE = 276
-   };
-#
+enum yytokentype {
+	kSET = 258,
+	kHALT = 259,
+	kJUMP = 260,
+	kJUMPT = 261,
+	rD = 262,
+	rIP = 263,
+	rREAD = 264,
+	rWRITE = 265,
+	rWRITELN = 266,
+	tINT = 267,
+	tSTRING = 268,
+	tNAME = 269,
+	tEQ = 270,
+	tNE = 271,
+	tGT = 272,
+	tLT = 273,
+	tGE = 274,
+	tLE = 275,
+	tNEWLINE = 276
+};
